e2de1e4aefda03599739a1e775d8300aafd33610
Alex <alex@alexanderkilty.me>
2013-01-19 10:20:42 +0200

    Initial Commit

---
+++ MovieApp/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MovieApp
+//
+//  Created by Myth James on 1/19/13.
+//  Copyright (c) 2013 Myth James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MovieApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MovieApp
+//
+//  Created by Myth James on 1/19/13.
+//  Copyright (c) 2013 Myth James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
